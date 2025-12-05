37d97948e5d4741ee35943be3f46d7c1fe7dec11
del1a <anacirebon12345@gmail.com>
2025-12-05 14:42:07 +0700

    Fix PHP file formatting and connection code

--- koneksi.php
+++ koneksi.php
@@ -4,10 +4,11 @@ $user = "root";
 $password = "";
 $nama_db = "db_bale";
 
-$koneksi = mysqli_connect("localhost","root","","db_bale");
+$koneksi = mysqli_connect ("localhost","root","","db_bale");
 
 if( !$koneksi){
     die("Gagal terhubung dengan database:" .mysqli_connect_error());
 }
+
 
 ?>
