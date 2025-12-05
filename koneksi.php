b3e27ff19cde422436a2b8d5a83a31b86423396b
del1a <anacirebon12345@gmail.com>
2025-12-05 14:34:40 +0700

    tugas5
    buatan adel

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$nama_db = "db_bale";
+
+$koneksi = mysqli_connect("localhost","root","","db_bale");
+
+if( !$koneksi){
+    die("Gagal terhubung dengan database:" .mysqli_connect_error());
+}
+
+?>
